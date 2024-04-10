da7630adf6cc3743bc9b2a4bdbbde503e28eb0e1
captainsparrow10 <javier1009rm@gmail.com>
2024-04-10 16:00:29 -0500

    loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,8 @@
+import Home from "@/components/home";
 import { getMemeImages } from "@/services/meme";
-import { PhotoIcon } from "@heroicons/react/16/solid";
-import Image from "next/image";
-import Link from "next/link";
 
-export default async function Home() {
-  const memes = await getMemeImages();
-  if (memes === undefined) {
-    return;
-  }
-  return (
-    <main className="flex flex-col gap-y-6 p-6">
-      <header className="flex items-center justify-between">
-        <h1 className="text-4xl font-bold">Imagenes</h1>
-        <PhotoIcon className="h-6 w-6" />
-      </header>
-      <section className="flex flex-wrap gap-3">
-        {memes.map(({ id, url, width, height, name }) => (
-          <Link href={`/${id}`} key={id}>
-            <div className="h-full w-full max-w-[150px]">
-              <Image src={url} width={width} height={height} alt={name} />
-            </div>
-          </Link>
-        ))}
-      </section>
-    </main>
-  );
+export default async function HomePage() {
+  const meme = await getMemeImages({ prevPage: 0 });
+
+  return <Home meme={meme} />;
 }
--- src/services/meme.ts
+++ src/services/meme.ts
@@ -1,14 +1,38 @@
 import { memeImageType } from "@/types";
+import { Dispatch, SetStateAction } from "react";
 
-export const getMemeImages = async (): Promise<memeImageType[] | undefined> => {
+type Props = {
+  prevPage?: number
+  newPreview?: Dispatch<SetStateAction<number>>
+}
+
+export const getMemeImages = async ({ prevPage = 0, newPreview }: Props): Promise<memeImageType[] | undefined> => {
   try {
     const response = await fetch('https://api.imgflip.com/get_memes');
     const data = await response.json();
-    const meme = data.data.memes
-    return meme;
+    const memes = data.data.memes;
+    const totalMemes = memes.length;
+    let currentMemes: memeImageType[] = [];
+    let nextPage = prevPage + 10
+    console.log(nextPage)
+    console.log(prevPage <= totalMemes)
+    if (prevPage <= totalMemes) {
+      if (newPreview) {
+        newPreview(nextPage)
+      }
+      const loadNextMemes = async () => {
+        const endIndex = Math.min(prevPage + nextPage, totalMemes);
+        console.log(endIndex)
+        const nextMemes = memes.slice(prevPage, endIndex);
+        currentMemes = [...currentMemes, ...nextMemes];
+        return currentMemes;
+      };
+      return await loadNextMemes();
+    }
+    return currentMemes
   } catch (error) {
-    console.error('Error al obtener meme:', error);
-
+    console.error('Error al obtener memes:', error);
+    return undefined;
   }
 };
 
