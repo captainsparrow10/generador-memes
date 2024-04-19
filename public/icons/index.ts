2e4d20da17d095340045b31477c94b060785accb
Javier Acosta Tullock <javier1009rm@gmail.com>
2024-04-19 13:26:25 -0500

    Delete public/icons directory

--- public/icons/index.ts
+++
@@ -1,6 +0,0 @@
-import AddImage from "@Public/Icons/AddImage";
-import Random from "@Public/Icons/Random";
-import Sticker from "@Public/Icons/Sticker"
-
-
-export { AddImage as AddImageIcon, Random as RandomIcon, Sticker as StickerIcon }
