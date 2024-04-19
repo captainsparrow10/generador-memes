f2e9264690e0905220f5083d0f7767459ec6941e
Javier Acosta Tullock <javier1009rm@gmail.com>
2024-04-19 13:28:35 -0500

    Create index.ts

---
+++ src/Types/index.ts
@@ -0,0 +1,38 @@
+import { StaticImageData } from "next/image"
+import { SVGProps } from "react"
+
+export type MemeImageType = {
+	id: string
+	name: string
+	url: string
+	width?: number
+	height?: number
+	box_count?: number
+	captions?: number
+}
+
+export type StylesType = {
+	text: string
+	color: string;
+	fontSize: number;
+	fontFamily: string;
+	transform: "normal-case" | "uppercase";
+	weight: "bold" | "normal";
+	style: "italic" | "not-italic";
+};
+
+
+export type StickerType = {
+	id: number
+	image: StaticImageData
+	size: number
+}
+
+export type FilterImageType = {
+	color: string;
+	opacity: number;
+}
+
+export type SvgProps = {
+	className?: string | undefined;
+} & SVGProps<SVGSVGElement>
